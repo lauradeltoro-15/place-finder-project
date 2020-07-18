664ed6b1c86acd654c52bc0b0219e4ba73d38430
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-07-18 11:45:13 +0200

    changing routes and authentication

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 module.exports = app => {
 
   // Base URLS
-  app.use('/', require('./index.routes'))
-  app.use('/', require('./auth.routes'))
+  app.use('/', require('./base'))
+  app.use('/', require('./auth'))
 }
