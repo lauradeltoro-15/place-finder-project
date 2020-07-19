a8c66045867ba5f50899757c059884d68abe118c
Elena Sanchez Leon <elena.farma94@gmail.com>
2020-07-19 20:47:47 +0200

    event model added

---
+++ server/models/event.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const eventSchema = new Schema({
+
+    name: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 500
+    },
+    date: {
+            type: Date,
+            required: true
+    },
+    // capacity: {
+    //     type: Number,
+    //     required: true,
+    //     min: 2
+    // },
+    //location || ciudad?
+    city: {
+        type: String,
+        required: true
+    },
+    typeOfLocal: {
+        type: String,
+        enum: ["restaurant", "gym", "hotel", "others"]
+    },
+
+    offers: [{ type: Schema.Types.ObjectId, ref: "Offer" }],
+
+    local: {type: Schema.Types.ObjectId, ref: "Local"}
+
+}, {
+    timestamps: true
+})
+
+const Event = mongoose.model("Event", eventSchema)
+
+module.exports = Event
