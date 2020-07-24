f1d189096f55116167b5eddbdef00bb8dbb3acc0
Elena <elenaslmm94@gmail.com>
2020-07-24 19:41:36 +0200

    cloudinary

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ module.exports = app => {
   app.use('/api/user', require('./user'))
   app.use('/api/local', require('./local'))
   app.use('/api/offer', require('./offer'))
+  app.use('/api/files', require('./files'))
   app.use((req, res) => {
     res.sendFile(__dirname + "/public/index.html");
   })
