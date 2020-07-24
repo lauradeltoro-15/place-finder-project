2929388264fdf7c2166f3674914097cde7cea92b
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-07-24 15:03:22 +0200

    Ready for deploy

---
+++ client/src/components/pages/home-page/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./home-page.css"
+
+const HomePage = () => {
+
+    return (
+        <main className="hero">
+            <h1>Great plans?</h1>
+        </main>
+    )
+}
+
+export default HomePage
