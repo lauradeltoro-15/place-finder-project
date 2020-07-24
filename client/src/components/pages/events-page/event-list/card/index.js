93f8ab819e9b7dcfe8454a6d30eaf79d6dde07a1
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-07-24 18:47:43 +0200

    minor change

--- client/src/components/pages/events-page/event-list/card/index.js
+++ client/src/components/pages/events-page/event-list/card/index.js
@@ -73,13 +73,11 @@ class EventCard extends Component {
                     <Card.Body>
                         <Card.Title>{this.props.name}</Card.Title>
                         <Card.Subtitle>Creator: {this.state.owner}</Card.Subtitle>
-                        <Card.Text>Participants:{this.props.participants.length}</Card.Text>
-                        <Card.Text>City:{this.props.city}</Card.Text>
+                        <Card.Text>Participants: <span className="color-text">{this.props.participants.length}</span></Card.Text>
+                        <Card.Text>City: {this.props.city}</Card.Text>
                         <Card.Text>Date: {this.formatDate(this.props.startTime)}</Card.Text>
-                        <Card.Text>Start time: {this.formatHour(this.props.startTime)}</Card.Text>
-                        <Card.Text>End time: {this.formatHour(this.props.endTime)}</Card.Text>
+                        <Card.Text>From: {this.formatHour(this.props.startTime)} To: {this.formatHour(this.props.endTime)}</Card.Text>
                         <Card.Text>Type of Local: {this.props.typeOfLocal}</Card.Text>
-                        <Card.Text>Description: {this.props.description}</Card.Text>
                         {this.props.loggedInUser && this.props.loggedInUser._id === this.props.owner &&
                             <>
                                 <Button variant="primary" onClick={() => this.deleteEvent(this.props._id) && <Redirect to='/profile' />}>Delete</Button>
@@ -89,7 +87,7 @@ class EventCard extends Component {
                         {this.props.loggedInUser && this.props.loggedInUser._id !== this.props.owner && this.props.loggedInUser.personDetails &&
                             <Button variant={this.isParticipating() ? "danger" : "primary"} onClick={() => { this.isParticipating() ? this.leaveEvent(this.props._id, this.props.loggedInUser._id) : this.joinEvent(this.props._id, this.props.loggedInUser._id) }}>{this.isParticipating() ? "Leave event" : "Join event"} </Button>
                         }
-                        <Link to={`/user/${this.state.ownerId}/events/${this.props._id}`} ><Button variant="primary">See details</Button></Link>
+                        <Link to={`/user/${this.state.ownerId}/events/${this.props._id}`} ><Button variant="primary">More</Button></Link>
                         
                         {this.props.loggedInUser && this.props.loggedInUser.companyDetails &&
                             <Link to={`/user/${this.state.ownerId}/event/${this.props._id}/offer/add`} ><Button variant="primary">Add an offer</Button></Link>
