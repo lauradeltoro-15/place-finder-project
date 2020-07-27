62674fdf72ec408b8a93982d7d4db252becf8c8a
Elena <elenaslmm94@gmail.com>
2020-07-27 16:02:07 +0200

    pagination added

--- client/src/components/pages/events-page/event-list/index.js
+++ client/src/components/pages/events-page/event-list/index.js
@@ -6,21 +6,91 @@ import EventCard from './card'
 import Row from 'react-bootstrap/esm/Row'
 import Col from 'react-bootstrap/esm/Col'
 
+import ReactPaginate from 'react-paginate'
+import './pagination.css'
+
 class  EventList extends Component {
     constructor (props){
         super (props)
         this.state = {
+            events: this.props.events,
+            offset: 0,
+            elements: undefined,
+            perPage: 9,
+            currentPage: 0
         }
+
         this.eventService = new EventService()
     }
-    render() {
 
-        return (
-            <Row as="section" className="row-card-container">
-                {this.props.events.map(event => <Col md={4}><EventCard {...this.props} updateEventList={this.props.updateEventList} loggedInUser={this.props.loggedInUser} key={event._id} {...event} /></Col>)}
+    componentDidMount = () => this.setEvents()
+
+    setEvents = () => {
+        this.setState({pageCount: Math.ceil(this.state.events.length/this.state.perPage)}, ()=>this.setElementsForCurrentPage())
+    }
+
+
+    setElementsForCurrentPage = () => {
+        let elements = this.state.events.slice(this.state.offset, this.state.offset + this.state.perPage).map((event, i) => {
+            return (
+                <Col md={4}><EventCard key={i} {...this.props} updateEventList={this.props.updateEventList} loggedInUser={this.props.loggedInUser} key={event._id} {...event} /></Col>)}
+            )
+        this.setState({elements: elements})
+    }
+
+    handlePageClick = events => {
+        const selectedPage = events.selected;
+        const offset = selectedPage * this.state.perPage;
+        this.setState({ currentPage: selectedPage, offset: offset }, () => this.setElementsForCurrentPage())
+    }
+    
+    render() {
+        let paginationElement
+        if(this.state.pageCount > 1) paginationElement = (
+            <ReactPaginate
+              previousLabel={"← Prev.Page"}
+              nextLabel={"Next-Page →"}
+              breakLabel={<span className="gap">...</span>}
+              pageCount={this.state.pageCount}
+              onPageChange={this.handlePageClick}
+              forcePage={this.state.currentPage}
+              containerClassName={"pagination justify-content-right align-items-center color-black"}
+              pageClassName={"page-link"}
+              previousClassName={"page-link"}
+              previousLinkClassName={"page-item"}
+              nextClassName={"page-link"}
+              nextLinkClassName={"page-item"}
+              disabledClassName={"disabled"}
+              activeClassName={"page-item active"}
+              activeLinkClassName={"page-link"}
+            />
+          ) 
+
+    return (
+        <>
+            {paginationElement}
+            <Row>    
+
+                {this.state.elements}
+            
             </Row>
-        )
+            {paginationElement}
+        </>
+          )
     }
 }
+    
+    
+
+
+    // render() {
+
+    //     return (
+    //         <Row as="section" className="row-card-container">
+    //             {this.state.events.map(event => <Col md={4}><EventCard {...this.props} updateEventList={this.props.updateEventList} loggedInUser={this.props.loggedInUser} key={event._id} {...event} /></Col>)}
+    //         </Row>
+    //     )
+    // }
+
 
 export default EventList
--- client/src/components/pages/events-page/index.js
+++ client/src/components/pages/events-page/index.js
@@ -64,21 +64,21 @@ class EventPage extends Component {
     getKilometros = function(lat1,lon1,lat2,lon2)
     {
         console.log('coordenadas a evaluar', lat1,lon1,lat2,lon2)
-    const rad = (deg)  => deg* (Math.PI/180)
-    const R = 6378.137; //Radio de la tierra en km
-    const dLat = rad( lat2 - lat1 );
-    const dLong = rad( lon2 - lon1 );
-    const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(rad(lat1)) * Math.cos(rad(lat2)) * Math.sin(dLong/2) * Math.sin(dLong/2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    const d = R * c;
-    return d*1000 //distance in m!
+        const rad = (deg)  => deg* (Math.PI/180)
+        const R = 6378.137; //Radio de la tierra en km
+        const dLat = rad( lat2 - lat1 );
+        const dLong = rad( lon2 - lon1 );
+        const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(rad(lat1)) * Math.cos(rad(lat2)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        const d = R * c;
+        return d*1000 //distance in m!
     }
 
 
 
     render() {
 
-        this.state.confirmedEvents && console.log(`la distancia entre los puntos es de: `, this.getKilometros(40.1, 3.7, 40.100002, 3.695)) 
+        //this.state.confirmedEvents && console.log(`la distancia entre los puntos es de: `, this.getKilometros(40.1, 3.7, 40.100002, 3.695)) 
        
         return (
             <>
